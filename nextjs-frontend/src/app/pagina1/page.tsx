0f4130647bf8f99f8def3ac87a690b5aaabe2358
wesjps <wesleyjps@yahoo.com.br>
2024-02-01 09:42:01 -0300

    💄 style: Theme creation

---
+++ nextjs-frontend/src/app/pagina1/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material"
+
+function Pagina1(){
+    return(
+        <div>
+            <h1>Página 1 teste</h1>
+            <Button variant="contained" color="primary">Botão</Button>
+        </div>
+    )
+}
+
+export default Pagina1
